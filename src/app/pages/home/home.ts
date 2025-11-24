a01fe8cd36c38cdc6ab73f7b0aef9793cab59bb6
Pedro <oliveirasilvapedro2@gmail.com>
2025-11-24 18:02:40 -0300

    finalizacao

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Hero } from '../../components/hero/hero';
 import { Services } from '../../components/services/services';
@@ -7,6 +7,8 @@ import { Contato } from '../../components/contato/contato';
 import { ClientOpinion } from '../../components/client-opinion/client-opinion';
 import { Ad } from '../../components/ad/ad';
 import { Footer } from '../../components/footer/footer';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 @Component({
   selector: 'app-home',
@@ -14,6 +16,22 @@ import { Footer } from '../../components/footer/footer';
   styleUrl: './home.scss',
   imports: [Header, Hero, Services, SeeClients, Contato, ClientOpinion, Ad, Footer],
 })
-export class Home {
+export class Home implements AfterViewInit{
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray(".anim").forEach((el: any) => {
+      gsap.from(el, {
+        scrollTrigger: {
+          trigger: el,
+          start: "top 100%",
+        },
+        opacity: 0,
+        x: -150,         
+        duration: 1.5,
+        ease: "power2.out"
+      });
+    });
+  }
 
 }
